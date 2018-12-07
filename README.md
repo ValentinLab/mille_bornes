68faa0dafc37ecd69bebce943cca2be1cfd69ad2
Valentin P <valentin.perignon@orange.fr>
2018-12-07 10:47:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Jeu du Mille Bornes
+
+Jeu du Mille Bornes réalisé en Java. Décembre 2018.
+
+## Licence
+GNU v3.0
---
+++ src/MilleBorne.java
@@ -0,0 +1,17 @@
+/**
+ * Jeu du Mille Bornes (décembre 2018) <br>
+ * MilleBornes.java
+ *
+ * @author Valentin P
+ * @version 1.0
+ */
+
+public class MilleBorne {
+	// ******************************
+	//  Main
+	// ******************************
+
+	public static void main(String args[]) {
+
+	}
+}
